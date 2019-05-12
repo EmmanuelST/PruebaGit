81bdf5d3d58ccaa193939f25374b659376b0f419
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-05-11 21:38:49 -0400

    Subiendo el resto

---
+++ Prueba3/Ejercicios/Program2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cola pila = new Cola();
+
+            pila.agregar(5);
+            pila.agregar(4);
+            pila.agregar(3);
+            pila.agregar(2);
+            pila.agregar(1);
+
+            pila.listar();
+
+            Console.ReadKey();
+
+        }
+    }
+}
